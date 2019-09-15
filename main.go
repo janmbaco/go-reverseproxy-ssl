a73568aab28b03c761af7df3e247e8240deeae62
José Ángel Navarro Martínez <janmbaco@gmail.com>
2019-09-15 15:25:14 +0200

    * Add Header to pass PK Certificate (as a token)

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
+	"fmt"
 	"github.com/janmbaco/go-reverseproxy-ssl/cross"
 	"github.com/janmbaco/go-reverseproxy-ssl/disk"
 	"github.com/janmbaco/go-reverseproxy-ssl/servers"
 	"golang.org/x/crypto/acme/autocert"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type(
@@ -17,6 +25,10 @@ type(
 		Scheme string `json:"scheme"`
 		HostName string `json:"host_name"`
 		Port uint `json:"port"`
+		CaPem string `json:"ca_pem"`
+		ClientCrt string `json:"client_crt"`
+		ClientKey string `json:"client_key"`
+		NeedPKFromClient bool `json:"need_pk_from_client"`
 	}
 
 	config struct{
@@ -46,8 +58,19 @@ func main() {
 		LogConsoleLevel:  cross.Trace,
 		LogFileLevel:     cross.Warning,
 	}
+	// añadimos el constructroContenido
+	disk.ConfigFile.ConstructorContent = func() interface{}{
+			return &config{}
+	}
+
+	//añadimo la copia de config
+	disk.ConfigFile.CopyContent = func(from interface{}, to interface{}) {
+		fromConf := from.(*config)
+		toConf := to.(*config)
+		*toConf = *fromConf
+	}
 	//write o read config from file
-	disk.LoadConfig(conf)
+	disk.ConfigFile.Load(conf)
 
 	go func() {
 		mux := http.NewServeMux()
@@ -63,33 +86,153 @@ func main() {
 		cross.TryPanic(server.ListenAndServe())
 	}()
 
-	getProxy := func (scheme string, host string, port uint)  *httputil.ReverseProxy  {
-		remote, err := url.Parse(scheme + "://" + host + ":" + strconv.Itoa(int(port)))
+	defaultTransport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 20 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       2 * time.Second,
+		TLSHandshakeTimeout:   100 * time.Millisecond,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
+	getCertPool := func(caPems ...string) *x509.CertPool{
+		rootCAs, _ := x509.SystemCertPool()
+		if rootCAs == nil {
+			rootCAs = x509.NewCertPool()
+		}
+		for _, caPem := range caPems{
+			pem, err := ioutil.ReadFile(caPem)
+			cross.TryPanic(err)
+			rootCAs.AppendCertsFromPEM(pem)
+		}
+		return rootCAs
+	}
+
+	getProxy := func (remoteHost remoteHost, rw http.ResponseWriter, req *http.Request)   {
+		_, err := url.Parse(remoteHost.Scheme + "://" + remoteHost.HostName + ":" + strconv.Itoa(int(remoteHost.Port)))
 		cross.TryPanic(err)
-		return httputil.NewSingleHostReverseProxy(remote)
+		proxy := httputil.ReverseProxy{
+			Director:        func(outReq *http.Request) {
+				outReq.URL.Scheme = remoteHost.Scheme
+				outReq.URL.Host = remoteHost.HostName + ":" + strconv.Itoa(int(remoteHost.Port))
+				if remoteHost.NeedPKFromClient && req.TLS.PeerCertificates != nil {
+					pubKey := base64.URLEncoding.EncodeToString(req.TLS.PeerCertificates[0].RawSubjectPublicKeyInfo)
+					outReq.Header.Set("X-Forwarded-ClientKey", pubKey)
+				}
+			},
+			Transport:      nil,
+			FlushInterval:  0,
+			ErrorLog:       cross.Log.ErrorLogger,
+			BufferPool:     nil,
+			ModifyResponse: nil,
+			ErrorHandler:   nil,
+		}
+		//Add transport tls layer
+		transport := defaultTransport
+		if len(remoteHost.CaPem) > 0 {
+			transport.TLSClientConfig = &tls.Config{
+				RootCAs: getCertPool(remoteHost.CaPem),
+			}
+		}
+		//add client certificates
+		if remoteHost.NeedPKFromClient && req.TLS.PeerCertificates != nil && len(remoteHost.ClientKey) > 0 && len(remoteHost.ClientCrt) > 0 {
+			clientCert, err := tls.LoadX509KeyPair(remoteHost.ClientCrt, remoteHost.ClientKey)
+			cross.TryPanic(err)
+			transport.TLSClientConfig.Certificates=  []tls.Certificate{clientCert}
+		}
+
+		proxy.Transport = transport
+		proxy.ServeHTTP(rw, req)
 	}
 
 	servers.NewListener(
 		func(httpServer *http.Server)  {
+			cross.Log.SetConsoleLevel(conf.LogConsoleLevel)
+			cross.Log.SetFileLogLevel(conf.LogFileLevel)
+
 			mux := http.NewServeMux()
-			//redirect to web host with www.
-			mux.Handle(strings.Replace(conf.DefaultHost, "www.","", 1)+"/",
-				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					http.Redirect(w, r, "https://"+conf.DefaultHost+r.RequestURI, http.StatusMovedPermanently)
-				}))
+
+			if conf.DefaultHost != "localhost" {
+				//redirect to web host with www.
+				mux.Handle(strings.Replace(conf.DefaultHost, "www.", "", 1)+"/",
+					http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						http.Redirect(w, r, "https://"+conf.DefaultHost+r.RequestURI, http.StatusMovedPermanently)
+					}))
+			}
 			var virtualHost []string
-			for name, vhost := range conf.VirtualHost{
+			var caPems []string
+			for name, vHost := range conf.VirtualHost{
 				virtualHost = append(virtualHost, name)
-				mux.Handle(name + "/", getProxy(vhost.Scheme, vhost.HostName, vhost.Port))
+				if len(vHost.CaPem) > 0 {
+					caPems = append(caPems, vHost.CaPem)
+				}
+				cross.Log.Info(fmt.Sprintf("register proxy from: '%v' to '%v://%v:%v'", name,vHost.Scheme, vHost.HostName, vHost.Port))
+				mux.HandleFunc(name+"/", func(w http.ResponseWriter, r *http.Request) {
+					getProxy(vHost, w, r)
+				})
 			}
-			autocert := &autocert.Manager{
-				Cache:      autocert.DirCache("certs"),
-				Prompt:     autocert.AcceptTOS,
-				HostPolicy: autocert.HostWhitelist(virtualHost...),
+
+			getTlsConfig := func () *tls.Config{
+				ret := &tls.Config{}
+				if conf.DefaultHost == "localhost" {
+					//in localhost doesn't works autocert
+					cert, err := tls.LoadX509KeyPair("./certs/server.crt", "./certs/server.key")
+					cross.TryPanic(err)
+
+					ret = &tls.Config{
+						Rand:                  rand.Reader,
+						Time:                  nil,
+						Certificates:          []tls.Certificate{cert},
+						NameToCertificate:     nil,
+						GetCertificate:        nil,
+						GetClientCertificate:  nil,
+						GetConfigForClient:    nil,
+						VerifyPeerCertificate: nil,
+						RootCAs:               nil,
+						NextProtos:            nil,
+						ServerName:            "",
+						ClientCAs:             nil,
+						InsecureSkipVerify:    false,
+						CipherSuites: []uint16{
+							tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+							tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+							tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+							tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+						},
+						PreferServerCipherSuites:    true,
+						SessionTicketsDisabled:      false,
+						SessionTicketKey:            [32]byte{},
+						ClientSessionCache:          nil,
+						MinVersion:                  tls.VersionTLS12,
+						MaxVersion:                  0,
+						CurvePreferences:            []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+						DynamicRecordSizingDisabled: false,
+						Renegotiation:               0,
+						KeyLogWriter:                nil,
+					}
+				} else {
+					autocert := &autocert.Manager{
+						Prompt:          autocert.AcceptTOS,
+						Cache:           autocert.DirCache("certs"),
+						HostPolicy:      autocert.HostWhitelist(virtualHost...),
+					}
+
+					ret = autocert.TLSConfig()
+				}
+				ret.ClientAuth=tls.VerifyClientCertIfGiven
+				ret.ClientCAs = getCertPool(caPems...)
+				return ret
 			}
+			httpServer.ReadTimeout = 1 * time.Second
+			httpServer.WriteTimeout = 20 * time.Second
 			httpServer.Addr = conf.ReverseProxyPort
 			httpServer.Handler = mux
-			httpServer.TLSConfig = autocert.TLSConfig()
+			httpServer.TLSConfig = getTlsConfig()
+			httpServer.TLSNextProto = make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0)
 		}).Start()
 }
 
